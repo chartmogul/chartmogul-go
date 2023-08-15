a0b53d2e27fb6f06eb4be4f3ac67a75d6b3a623c
Paul Ryan <paul@chartmogul.com>
2023-08-15 14:06:30 +0100

    Add a filter which removes Authorization headers from the recorded request using go-vcr

--- integration_tests/create_invoice_test.go
+++ integration_tests/create_invoice_test.go
@@ -3,6 +3,7 @@ package integration
 import (
 	"log"
 	"net/http"
+	"os"
 	"reflect"
 	"testing"
 	"time"
@@ -10,8 +11,6 @@ import (
 	cm "github.com/chartmogul/chartmogul-go/v3"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/parnurzeal/gorequest"
-
-	"github.com/dnaeon/go-vcr/recorder"
 )
 
 func TestCreateInvoice(t *testing.T) {
@@ -19,14 +18,16 @@ func TestCreateInvoice(t *testing.T) {
 		t.Skip("Integration test.")
 	}
 
-	r, err := recorder.New("./fixtures/create_invoice")
+	r, err := NewRecorderWithAuthFilter("./fixtures/create_invoice")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer r.Stop() //nolint
 
-	api := &cm.API{}
-	api.SetClient(&http.Client{Transport: r})
+	api := &cm.API{
+		ApiKey: os.Getenv("CHARTMOGUL_API_KEY"),
+		Client: &http.Client{Transport: r},
+	}
 	gorequest.DisableTransportSwap = true
 
 	ds, err := api.CreateDataSource("Test Create Invoice")
--- integration_tests/retrieve_metrics_test.go
+++ integration_tests/retrieve_metrics_test.go
@@ -3,11 +3,11 @@ package integration
 import (
 	"log"
 	"net/http"
+	"os"
 	"testing"
 
 	cm "github.com/chartmogul/chartmogul-go/v3"
 	"github.com/davecgh/go-spew/spew"
-	"github.com/dnaeon/go-vcr/recorder"
 	"github.com/go-test/deep"
 	"github.com/parnurzeal/gorequest"
 )
@@ -17,14 +17,16 @@ func TestRetrieveMetrics(t *testing.T) {
 		t.Skip("Integration test.")
 	}
 
-	r, err := recorder.New("./fixtures/retrieve_metrics")
+	r, err := NewRecorderWithAuthFilter("./fixtures/retrieve_metrics")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer r.Stop() //nolint
 
-	api := &cm.API{}
-	api.SetClient(&http.Client{Transport: r})
+	api := &cm.API{
+		ApiKey: os.Getenv("CHARTMOGUL_API_KEY"),
+		Client: &http.Client{Transport: r},
+	}
 	gorequest.DisableTransportSwap = true
 
 	all := &cm.MetricsResult{
